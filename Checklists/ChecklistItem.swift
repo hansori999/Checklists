8899734189eda226aa51b483bfb302b4ae4eaf5e
Myoung-Wan Koo <mwkoo9@gmail.com>
2019-03-30 17:30:42 +0900

    Align of Table View

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,19 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Myoung-Wan Koo on 30/03/2019.
+//  Copyright © 2019 Myoung-Wan Koo. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+    
+    /* For toggling */
+    func toggleChecked() {
+        checked = !checked
+    }
+}
