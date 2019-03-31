4cf3729075882c1a54efae53d7b2a5e465f2438c
Myoung-Wan Koo <mwkoo9@gmail.com>
2019-03-31 23:10:26 +0900

    TextField Stype

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -8,10 +8,12 @@
 
 import UIKit
 
-class AddItemViewController: UITableViewController {
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
 
     @IBOutlet weak var textField: UITextField!
     
+    
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -39,6 +41,23 @@ class AddItemViewController: UITableViewController {
                             willSelectRowAt indexPath: IndexPath)
         -> IndexPath? {
             return nil
+    }
+    
+    // MARK:- Text Field Delegates
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        let oldText = textField.text!
+        let stringRange = Range(range, in:oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange,
+                                                  with: string)
+        doneBarButton.isEnabled = !newText.isEmpty
+        return true
+    }
+    
+    /* Clear Button */
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        doneBarButton.isEnabled = false
+        return true
     }
 
 }
