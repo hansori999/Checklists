251b1f98dbdc7597f1c89fd75789b22139205029
Myoung-Wan Koo <mwkoo9@gmail.com>
2019-04-07 20:51:50 +0900

    AcceryButtonTapped

--- Checklists/AllListsViewController.swift
+++ Checklists/AllListsViewController.swift
@@ -109,4 +109,17 @@ class AllListsViewController: UITableViewController,ListDetailViewControllerDele
         navigationController?.popViewController(animated: true)
     }
 
+    /* For accessoryButtonTapped Case */
+    override func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
+        
+        let controller = storyboard!.instantiateViewController(withIdentifier: "ListDetailViewController") as! ListDetailViewController
+        controller.delegate = self
+        
+        let checklist = lists[indexPath.row]
+        controller.checklistToEdit = checklist
+        
+        navigationController?.pushViewController(controller, animated: true)
+    }
+    
+
 }
