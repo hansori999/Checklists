d99df0584443db017ce5a031a76e3e5d7d5b599d
Myoung-Wan Koo <mwkoo9@gmail.com>
2019-04-15 01:55:00 +0900

    Adding a to-do items

--- Checklists/DataModels/ChecklistItem.swift
+++ Checklists/DataModels/ChecklistItem.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UserNotifications
 
 class ChecklistItem: NSObject, Codable {
     var text = ""
@@ -28,7 +29,23 @@ class ChecklistItem: NSObject, Codable {
     
     func scheduleNotification() {
         if shouldRemind && dueDate > Date() {
-            print(" We should schedule a notification!")
-        }
+            
+            let content = UNMutableNotificationContent()
+            content.title = "Reminder:"
+            content.body = text
+            content.sound = UNNotificationSound.default
+            
+            let calendar = Calendar(identifier: .gregorian)
+            let components = calendar.dateComponents([.month, .day, .hour, .minute], from: dueDate)
+            
+            let trigger = UNCalendarNotificationTrigger(dateMatching: components, repeats: false)
+            
+            let request = UNNotificationRequest(identifier: "\(itemID)", content: content, trigger: trigger)
+            
+            let center = UNUserNotificationCenter.current()
+            center.add(request)
+            
+            print("Scheduled: \(request) for itemID: \(itemID)")    }
+
     }
 }
--- Checklists/ViewControllers/ItemDetailViewController.swift
+++ Checklists/ViewControllers/ItemDetailViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 /* Protocol Defintion */
 protocol ItemDetailViewControllerDelegate: class {
@@ -88,6 +89,18 @@ class ItemDetailViewController: UITableViewController, UITextFieldDelegate {
         
     }
     
+    @IBAction func shouldRemindToggled(_ switchControl: UISwitch) {
+        textField.resignFirstResponder()
+        
+        if switchControl.isOn {
+            let center = UNUserNotificationCenter.current()
+            center.requestAuthorization(options: [.alert, .sound]) {
+                granted, error in
+                // do nothing
+            }
+        }
+    }
+
     @IBAction func dateChanged(_ datePicker: UIDatePicker) {
         dueDate = datePicker.date
         updateDueDateLabel()
