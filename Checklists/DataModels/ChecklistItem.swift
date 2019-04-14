d63eaaf717eb044ee1a843951e8f586724e3b104
Myoung-Wan Koo <mwkoo9@gmail.com>
2019-04-14 11:02:33 +0900

    Associate checklistItem object with local notification

--- Checklists/DataModels/ChecklistItem.swift
+++ Checklists/DataModels/ChecklistItem.swift
@@ -12,6 +12,15 @@ class ChecklistItem: NSObject, Codable {
     var text = ""
     var checked = false
     
+    var dueDate = Date()
+    var shouldRemind = false
+    var itemID = -1
+    
+    override init() {
+        super.init()
+        itemID = DataModel.nextChecklistItemID()
+    }
+
     /* For toggling */
     func toggleChecked() {
         checked = !checked
